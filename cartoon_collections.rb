684144528aa09115623069c3a77b6a827692ada4
Abagail Hudson <abagailanne@gmail.com>
2019-05-08 01:46:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |call| call.length >= 4 }
-  calls.any? { |call| call.length <= 3 }
-  calls.all? { |call| call.length > 4 }
+  calls.any? do |x|
+   x.length > 4
+ end
 end
 
 def find_the_cheese(strings)
